47dfe3dc6151e7571bac42dddc828e17618877b7
xJDvmPWSH <juandiegovaldesm03@gmail.com>
2024-09-17 02:21:47 -0400

    refactor: Update AuthGuard to increase token expiration time

--- src/users/dto/create-profile.dto.ts
+++ src/users/dto/create-profile.dto.ts
@@ -1,41 +1,70 @@
-import { IsString, IsDate, IsEmail, IsNotEmpty, ValidateNested, IsNumber } from 'class-validator';
+import { IsString, IsNumber, IsOptional, ValidateNested, IsDate, IsEmail } from 'class-validator';
 import { Type } from 'class-transformer';
 
 class AddressDto {
+  @IsString()
+  @IsOptional()
   name: string;
+
+  @IsString()
+  @IsOptional()
   state: string;
+
+  @IsString()
+  @IsOptional()
   country: string;
+
+  @IsString()
+  @IsOptional()
   city: string;
+
+  @IsNumber()
+  @IsOptional()
   lat: number;
+
+  @IsNumber()
+  @IsOptional()
   lon: number;
+
+  @IsNumber()
+  @IsOptional()
   postcode: number;
+
+  @IsString()
+  @IsOptional()
   street: string;
 }
 
 export class CreateProfileDto {
-
   @IsString()
   username: string;
 
   @IsString()
+  @IsOptional()
   firstname?: string;
 
   @IsString()
+  @IsOptional()
   lastname?: string;
 
   @IsDate()
+  @IsOptional()
   birthdate?: Date;
 
   @IsString()
+  @IsOptional()
   gender?: string;
 
   @IsString()
+  @IsOptional()
   country?: string;
 
   @IsEmail()
+  @IsOptional()
   email?: string;
 
   @IsString()
+  @IsOptional()
   password?: string;
 
   @ValidateNested()
