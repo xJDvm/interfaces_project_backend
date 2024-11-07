d8c9ed0730a0c4c8252044c3f47839a425393482
Juan D. Valdés <jvaldes@intelix.biz>
2024-11-07 10:18:19 -0400

    Refactor profile.controller.ts to comment out unused imports and code related to file uploads

--- src/users/controllers/profile.controller.ts
+++ src/users/controllers/profile.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Patch, Param, Body, UseInterceptors, UploadedFile, ParseIntPipe } from '@nestjs/common';
-import { diskStorage } from 'multer';
-import { FileInterceptor } from '@nestjs/platform-express';
+// import { diskStorage } from 'multer';
+// import { FileInterceptor } from '@nestjs/platform-express';
 import { ProfileService } from '../services/profile.service';
 import { UpdateProfileDto } from '../dto/update-profile.dto';
 import { Profile } from '../entities/profile.entity';
 import { plainToClass } from 'class-transformer';
-import { Express } from 'express'; // Importar el tipo de Express para archivos
+// import { Express } from 'express'; // Importar el tipo de Express para archivos
 
 @Controller('profiles')
 export class ProfileController {
@@ -22,25 +22,25 @@ export class ProfileController {
   }
 
   @Patch(':id')
-  @UseInterceptors(FileInterceptor('image', {
-    storage: diskStorage({
-      destination: './uploads',
-      filename: (req, file, cb) => {
-        const filename = `${Date.now()}-${file.originalname}`;
-        cb(null, filename);
-      },
-    }),
-  }))
+  // @UseInterceptors(FileInterceptor('image', {
+  //   storage: diskStorage({
+  //     destination: './uploads',
+  //     filename: (req, file, cb) => {
+  //       const filename = `${Date.now()}-${file.originalname}`;
+  //       cb(null, filename);
+  //     },
+  //   }),
+  // }))
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() body: any,
-    @UploadedFile() file: Express.Multer.File,
+    // @UploadedFile() file: Express.Multer.File,
   ): Promise<Profile> {
     const updateProfileDto = plainToClass(UpdateProfileDto, body);
 
-    if (file) {
-      updateProfileDto.imagePath = file.path;
-    }
+    // if (file) {
+    //   updateProfileDto.imagePath = file.path;
+    // }
     if (typeof body.address === 'string') {
       updateProfileDto.address = JSON.parse(body.address);
     } else {
